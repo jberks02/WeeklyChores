59138848818f4454852fa744ced6a03d82fe77b5
Justin Berkshire <justinberkshire@Justins-MacBook-Pro.local>
2022-04-23 22:07:25 -0500

    increased font size and size of elements

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import './style.css';
 export const Header = () => {
     return (
        <Grid item xs={12} className='backgroundDiv' >
-           <Typography style={{margin: 0}} >Weekly Chores</Typography>
+           <Typography style={{margin: 0, fontSize: 40}} >Weekly Chores</Typography>
         </Grid>
     )
 }
